7fdc20fe2071bd6fdb15cfa663c19a2efa04a07a
datnt-16520200 <datnt.148@gmail.com>
2021-05-05 16:29:44 +0700

    edit server adress

--- src/server.js
+++ src/server.js
@@ -10,6 +10,6 @@ server.addService(notesProto.NoteService.service, {
     callback(null, notes)
   },
 })
-server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure())
+server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure())
 console.log('Server running at http://127.0.0.1:50051')
 server.start()
